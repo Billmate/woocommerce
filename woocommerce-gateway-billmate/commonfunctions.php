4c298fdb8a9d4834122e0113a3e4cfede6020e72
billmate-support1 <gagan@combain.com>
2014-09-22 14:46:16 +0530

    Fix: Updated Version
    Fix: Updated Version

--- woocommerce-gateway-billmate/commonfunctions.php
+++ woocommerce-gateway-billmate/commonfunctions.php
@@ -1,5 +1,5 @@
 <?php
-define('BILLPLUGIN_VERSION','1.20');
+define('BILLPLUGIN_VERSION','1.21');
 define('BILLMATE_VERSION','PHP:Woocommerce:'.BILLPLUGIN_VERSION);
 
 require_once(BILLMATE_LIB . 'BillMate.php');
