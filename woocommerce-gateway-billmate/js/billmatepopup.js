4d45e569b5d8d331b2f3c8ca605607caa93077ce
jesper@boxedlogistics.se <jesper@boxedlogistics.se>
2015-08-04 14:44:34 +0200

    Fixes billmatepopup again a dot to much
    Fixes #106
    Fixes #108
    Fixes #111

--- woocommerce-gateway-billmate/js/billmatepopup.js
+++ woocommerce-gateway-billmate/js/billmatepopup.js
@@ -344,7 +344,7 @@ AddEvent(window,'load',function(){
                 } else {
                     var message = '<div id="getaddresserror" class="woocommerce-error">'+result.message+'</div>';
                     $('#getaddresserr').html(message);
-                    $('[name="pno"]').parent.('p').removeClass('woocommerce-validated').addClass('woocommerce-invalid woocommerce-invalid-required-field');
+                    $('[name="pno"]').parent('p').removeClass('woocommerce-validated').addClass('woocommerce-invalid woocommerce-invalid-required-field');
                 }
             }
         })
