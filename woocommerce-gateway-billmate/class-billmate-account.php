a95bbd6252edec6fc257acdb5dbac9c00a0b5ef9
Boxedsolutions <jesper@boxedlogistics.se>
2015-08-18 10:16:48 +0200

    Prettified the output if no email is input

--- woocommerce-gateway-billmate/class-billmate-account.php
+++ woocommerce-gateway-billmate/class-billmate-account.php
@@ -810,7 +810,7 @@ class WC_Gateway_Billmate_Partpayment extends WC_Gateway_Billmate {
 			<div class="clear"></div>
 
 <?php
-$datatemp = array('billing_email'=>' ');
+$datatemp = array('billing_email'=>'');
 if(!empty($_POST['post_data'])){
 parse_str($_POST['post_data'], $datatemp);
 }
@@ -818,7 +818,7 @@ parse_str($_POST['post_data'], $datatemp);
 		<div class="clear"></div>
 			<p class="form-row">
 				<input type="checkbox" class="input-checkbox" checked="checked" value="yes" name="valid_email_it_is" id="valid_email_it_is" style="float:left;margin-top:6px" />
-				<label><?php echo sprintf(__('My e-mail ,%s, is correct och and may be used for billing. I confirm the ', 'billmate'), $datatemp['billing_email']) ?><a id="billmate_partpayment" href="javascript://"><?php echo __('terms of partpayment','billmate'); ?></a> <?php echo __('and accept the liability.','billmate') ?></label>
+				<label><?php echo sprintf(__('My e-mail%s is correct och and may be used for billing. I confirm the ', 'billmate'), (strlen($datatemp['billing_email']) > 0) ? ', '.$datatemp['billing_email'].',' : ' '); ?><a id="billmate_partpayment" href="javascript://"><?php echo __('terms of partpayment','billmate'); ?></a> <?php echo __('and accept the liability.','billmate') ?></label>
 			</p>
 
 			<?php if ( $this->shop_country == 'DE' && $this->de_consent_terms == 'yes' ) : ?>
