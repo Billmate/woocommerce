3e634b965dfb9136d610d3ee857ae70841145b0b
Paul Herrström <kalorifri@gmail.com>
2018-06-13 15:14:27 +0200

    use getCartShippingData in updateCheckout

--- woocommerce-gateway-billmate/class-billmate-checkout.php
+++ woocommerce-gateway-billmate/class-billmate-checkout.php
@@ -1035,33 +1035,12 @@ class WC_Gateway_Billmate_Checkout extends WC_Gateway_Billmate
 
         // Shipping
         unset($orderValues['Cart']);
-        if(version_compare(WC_VERSION, '3.0.0', '>=')) {
-            $order_shipping_total = $order->get_shipping_total();
-            $order_shipping_tax = $order->get_shipping_tax();
-        } else {
-            $order_shipping_total = $order->order_shipping;
-            $order_shipping_tax = $order->order_shipping_tax;
+        $shipping = $billmateOrder->getCartShipping();
+        if ($shipping['price'] > 0) {
+            $orderValues['Cart']['Shipping'] = $billmateOrder->getCartShippingData();
+            $total      += $shipping['price'];
+            $totalTax   += $shipping['tax'];
         }
-
-        if ($order_shipping_total > 0) :
-            // We manually calculate the shipping taxrate percentage here
-            $calculated_shipping_tax_percentage = ($order_shipping_tax / $order_shipping_total) * 100; //25.00
-            $calculated_shipping_tax_decimal = ($order_shipping_tax / $order_shipping_total) + 1; //0.25
-
-            // apply_filters to Shipping so we can filter this if needed
-            $billmate_shipping_price_including_tax = $order_shipping_total * $calculated_shipping_tax_decimal;
-            $shipping_price = apply_filters( 'billmate_shipping_price_including_tax', $billmate_shipping_price_including_tax );
-
-            $orderValues['Cart']['Shipping'] = array(
-                'withouttax'    => ($shipping_price - $order_shipping_tax) * 100,
-                'taxrate'      => round($calculated_shipping_tax_percentage),
-
-            );
-            $total += ($shipping_price - $order_shipping_tax) * 100;
-            $totalTax += (($shipping_price - $order_shipping_tax) * ($calculated_shipping_tax_percentage/100)) * 100;
-        endif;
-
-
 
         $round = (round($order->get_total() * 100)) - round($total + $totalTax,0);
 
