ac19fa38cc2158c70890ed54603ca4f9c63eb211
henrik-ecomatic <henrik@ecomatic.se>
2019-08-23 11:07:36 +0200

    WPM-21 [WIP] removed calls for aditional products on product page, only using main product. added parameter to disable caching in api

--- woocommerce-gateway-billmate/shortcodes.php
+++ woocommerce-gateway-billmate/shortcodes.php
@@ -58,10 +58,15 @@ function return_billmate_price() {
         'currency' => $currency,
         'language' => $language[0],
         'country' => $country,
-        'totalwithtax' => $price * 100
+        'totalwithtax' => $price * 100,
+        "time" => time()
     );
 
     $pclasses = $bm->getPaymentPlans($values);
+    ob_start();
+    var_dump($values);
+    var_dump($pclasses);
+    file_put_contents("bmdev.log", "2: " . ob_get_clean() . "\n", FILE_APPEND);
 	$flag = BillmateFlags::CHECKOUT_PAGE;
 	$pclasses_not_available = true;
 
