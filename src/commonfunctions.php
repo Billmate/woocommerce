c0acdb247dcf429fe25c612093136ac157e5dbea
Stefan Lidén <stefanliden123@hotmail.com>
2020-01-15 13:16:31 +0100

    Update to 3.9.1

--- src/commonfunctions.php
+++ src/commonfunctions.php
@@ -1,5 +1,5 @@
 <?php
-define('BILLPLUGIN_VERSION','3.9.0');
+define('BILLPLUGIN_VERSION','3.9.1');
 define('BILLMATE_CLIENT','PHP:Woocommerce:'.BILLPLUGIN_VERSION);
 define('BILLMATE_SERVER','2.1.9');
 
