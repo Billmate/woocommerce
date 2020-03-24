b64f7a05c37d6ed97f3575572982e7ee7bf3374b
Mattias Nilsson <mattias.nilsson@billmate.se>
2020-03-24 17:11:07 +0100

    [!!!][TASK] Add Typescript to Cypress tests
    Installation:
    * Run npm install

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,10 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const webpack = require('@cypress/webpack-preprocessor');
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+module.exports = on => {
+    const options = {
+        webpackOptions: require('../../webpack.config'),
+        watchOptions: {}
+    }
 
-module.exports = (on, config) => {
-
-};
+    on('file:preprocessor', webpack(options));
+}
