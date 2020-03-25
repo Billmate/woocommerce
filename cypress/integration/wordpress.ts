b5751ca698712b2a92b697d7b124cdde21910f0e
Mattias Nilsson <tollepjaer@gmail.com>
2020-03-25 07:58:52 +0100

    Update wordpress.ts

--- cypress/integration/wordpress.ts
+++ cypress/integration/wordpress.ts
@@ -4,11 +4,4 @@ describe('Wordpress', () => {
         expect(cy.visit(Cypress.env('host'))).to.be.ok;
     });
 
-    /*it('Login', function() {
-        cy.visit('https://online.billmate.se');
-        cy.get('#email').should('be.visible').type('oskar.ostlund');
-        cy.get('#password').should('be.visible').type('Mark1Mark2');
-        cy.get('#loginForm .buttonshort').click();
-    });*/
-
 });
