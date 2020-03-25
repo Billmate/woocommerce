a564116e6cec635fae5ce0c4d47185038cf99598
Mattias Nilsson <mattias.nilsson@billmate.se>
2020-03-25 08:47:38 +0100

    [TASK] Minor changes

--- bin/restore-db.sh
+++ bin/restore-db.sh
@@ -7,5 +7,5 @@ if [ -z "$file" ]; then
 fi
 
 # Restore database to db container
-cmd='exec mysql -P 3306 --protocol=tcp -h localhost -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE"'
+cmd='exec mysql -P 3306 --protocol=tcp -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE"'
 docker exec -i $(docker-compose ps -q db) sh -c "$cmd" < $file
