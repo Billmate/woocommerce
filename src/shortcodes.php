cff8734a880ada0d467a1cbc2057f5db4dc3508c
henrik-ecomatic <henrik@ecomatic.se>
2020-04-02 10:34:22 +0200

    WPM-129 [BUGFIX] updated ifstatmenet after adding checkout as an available gateway, ifstatement now chesks if there is more than one instead of more than zero

--- src/shortcodes.php
+++ src/shortcodes.php
@@ -127,7 +127,7 @@ function get_billmate_checkout() {
 
         // Button to WooCommerce checkout page when another payment method is available
         $available_gateways = WC()->payment_gateways->get_available_payment_gateways();
-        if (is_array($available_gateways) AND count($available_gateways) > 0) {
+        if (is_array($available_gateways) AND count($available_gateways) > 1) {
             $return .= '<div class="billmate-checkout-another-payment-wrapper">
                 <a class="button" href="'.get_permalink(wc_get_page_id('checkout')).'">'.__('Select another payment method', 'billmate').'</a>
             </div>';
